9475849f2d3c4e85c42feb3f66a45fe9c143d89c
jacobreesgit <jacobrees@me.com>
2025-07-01 16:58:52 +0100

    Initial Commit

---
+++ ChordBoard/ChordBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordBoardApp.swift
+//  ChordBoard
+//
+//  Created by Jacob Rees on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChordBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
